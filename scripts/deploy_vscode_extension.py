d1ade182e8b0c7ea2a9af7757c646f52e80bf6ce
Lionel Brouyere <lionel.brouyere@gmail.com>
2022-11-06 15:34:20 +0100

    SCRIPTS: improve deploy vscode extension

--- scripts/deploy_vscode_extension.py
+++ scripts/deploy_vscode_extension.py
@@ -1,13 +1,14 @@
 import os
 import shutil
 
+path = "~/AppData/Local/Programs/Microsoft VS Code/resources/app/extensions/vscode_extension"
 try:
-    shutil.rmtree(r"C:\Users\lio\AppData\Local\Programs\Microsoft VS Code\resources\app\extensions\vscode_extension")
+    shutil.rmtree(os.path.expanduser(path))
 except FileNotFoundError:
     pass
 
 corax_extension_path = os.path.join(os.path.dirname(__file__), '..', 'sdk', 'vscode_extension')
-vscode_extention_path = r"C:\Users\lio\AppData\Local\Programs\Microsoft VS Code\resources\app\extensions\vscode_extension"
+vscode_extention_path = os.path.expanduser(path)
 
 shutil.copytree(corax_extension_path, vscode_extention_path)
 
