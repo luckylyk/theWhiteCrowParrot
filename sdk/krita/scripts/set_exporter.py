3b6c42202335f4c31001e5c8c30365c2d2411328
Lionel Brouyère <lionel.brouyere@gmail.com>
2024-04-28 02:06:37 +0200

    WCP: Add mine_court_side_scene

--- sdk/krita/scripts/set_exporter.py
+++ sdk/krita/scripts/set_exporter.py
@@ -12,8 +12,8 @@ import json
 
 SRGB_PROFILE = "sRGB-elle-V2-srgbtrc.icc"
 FOLDER = r"C:\Users\lio\Desktop\zini\zut"
-LEVEL_FOLDER = "well_border"
-LEVEL_FILE = "well_border.json"
+LEVEL_FOLDER = "mine_court_side"
+LEVEL_FILE = "mine_court_side.json"
 DEFAULT_DATA = {
     "deph": 0.0,
     "visible": True,
